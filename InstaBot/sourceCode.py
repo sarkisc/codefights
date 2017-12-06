a4e51b96e82d2d3b3cd6408e26b405a01b7b9413
Sarkis Chouldjian <chouldjiansarkis@gmail.com>
2017-12-05 23:54:39 -0800

    beat InstaBot

--- InstaBot/sourceCode.py
+++ InstaBot/sourceCode.py
@@ -71,3 +71,97 @@ def isAdmissibleOverpayment(prices, notes, x):
     if abs(x - amountOverInStore) < 0.0000001 or x >= amountOverInStore:
         return True
     return False
+
+
+
+
+from datetime import datetime
+
+def busyHolidays(shoppers, orders, leadTime):
+    
+    if len(shoppers) < len(orders):
+        return False
+    
+    # for each order, figure out which shopper(s) can take the order
+    # 
+    # 
+    # didn't do these things
+    # if there exists an order that no shopper can take:
+        # return False
+    # if len(eligible_shoppers) < len(orders):
+        # return False
+    # 
+    # 
+    # permute through eligible shoppers for each order until a "good" combination is found
+        # return True
+    # if no "good" combination is found:
+        # return False
+    
+    shoppers_for_orders = [[] for i in range(len(orders))]
+
+    for i in range(len(orders)):
+        for j in range(len(shoppers)):
+            if canTakeOrder(orders[i], shoppers[j], leadTime[i]):
+                shoppers_for_orders[i].append(j)
+    
+    #print(shoppers_for_orders)
+    
+    sets = [set([num]) for num in shoppers_for_orders[0]]
+    i = 1
+    while i < len(orders) and sets != []:
+        sets = getNextLevel(sets, shoppers_for_orders[i], i+1)
+        i += 1
+    
+    if sets == []:
+        return False
+    return True
+    
+
+#helper
+def getNextLevel(sets, shoppers, desiredLength):
+    newSets = []
+    for st in sets:
+        for shopper in shoppers:
+            newSet = set(st)
+            newSet.add(shopper)
+            if len(newSet) == desiredLength:
+                newSets.append(newSet)
+    return newSets
+    
+
+# can shopper complete order? returns bool
+def canTakeOrder(order, shopper, leadTime):
+    
+    Ss = dt(shopper[0])
+    Se = dt(shopper[1])
+    Os = dt(order[0])
+    Oe = dt(order[1])
+    
+    if Ss > Oe:
+        return False
+    
+    shopperTime = -1
+    if Ss <= Os:
+        if Se <= Oe:
+            shopperTime = Se - Os
+        else:
+            shopperTime = Oe - Os
+    else:
+        if Se <= Oe:
+            shopperTime = Se - Ss
+        else:
+            shopperTime = Oe - Ss
+    
+    if shopperTime >= leadTime:
+        return True
+    return False
+
+
+# returns difference between two times in minutes
+# time1 must come after time2
+def dt(time1, time2="00:00"):
+    frmt = '%H:%M'
+    t1 = datetime.strptime(time1, frmt)
+    t2 = datetime.strptime(time2, frmt)
+    return (t1-t2).total_seconds() / 60
+    
